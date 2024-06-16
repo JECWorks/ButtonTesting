5c6855f96888427aa7f635de205fdba3b6918cb4
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-15 22:06:28 -0400

    Initial Commit

---
+++ ButtonTesting/ButtonTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTestingApp.swift
+//  ButtonTesting
+//
+//  Created by Jason Cox on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
