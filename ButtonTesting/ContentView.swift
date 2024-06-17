ae5c0331eff0ac1aefbeead1e7bb7e4a93d6266a
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-17 11:02:44 -0400

    Textfields added for name and focusing to appropriate first text field and then next.

--- ButtonTesting/ContentView.swift
+++ ButtonTesting/ContentView.swift
@@ -8,17 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedAge: Int = 18
+    @State private var votingMessage: String = "Register to Vote!"
+    @State private var textInput = ""
+    @State private var textInput2 = ""
+    @FocusState private var focus: FormFieldFocus?
+
+    enum FormFieldFocus: Hashable {
+        case firstname, lastname
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "highlighter")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            TextField("First Name", text: $textInput)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .onSubmit {
+                    print("Thanks for registering, \(textInput)")
+                }
+                .focused($focus, equals: .firstname)
+            
+            TextField("Last Name", text: $textInput2)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    .focused($focus, equals: .lastname)
+                         
+            Menu {
+                ForEach(15...25, id: \.self) { age in
+                    Button(action: { selectAge(age) }) {
+                        Text("\(age)")
+                    }
+                }
+            } label: {
+                Label("Choose your age", systemImage: "list.bullet")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .onAppear {
+                focus = .firstname
+            }
+            
+            
+            Text(votingMessage)
+                .foregroundColor(selectedAge >= 18 ? .green : .red)
+                .font(.title)
+                .padding()
         }
         .padding()
+        
+        
+    }
+    
+    
+
+    func selectAge(_ age: Int) {
+        selectedAge = age
+        if age >= 18 {
+            votingMessage = "\(textInput), you can vote!"
+        } else {
+            votingMessage = "\(textInput), you can't vote :("
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
